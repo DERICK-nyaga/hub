d28133d6c42f1be9071d368250669073d8e6b2dd
DERICK-nyaga <dericknyagam@gmail.com>
2026-05-01 14:21:58 +0300

    registered employee observer in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Observers\EmployeeObserver;
+use App\Models\Employee;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Employee::observe(EmployeeObserver::class);
     }
 
     /**
